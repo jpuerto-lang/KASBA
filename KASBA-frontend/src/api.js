1d6ff8b0965132c23b530479b92b7cd6b6cea3ca
P3P <jpuerto@xtec.cat>
2026-06-15 17:39:10 +0200

    Update api.js

--- KASBA-frontend/src/api.js
+++ KASBA-frontend/src/api.js
@@ -1,5 +1,5 @@
-const API_URL = 'http://192.168.3.10:3000'; 
-
+//const API_URL = 'http://192.168.3.10:3000'; 
+const API_URL = 'https://kasba-backend-production.up.railway.app'; // La teva URL real de Railway
 export function apiFetch(endpoint, options = {}) {
   const token = localStorage.getItem('token');
   const headers = {
